6fe336d399f6ffd073a3574db979e814b23cb2be
00julia <130668482+00julia@users.noreply.github.com>
2023-10-25 10:46:37 -0300

    Commit conversor

--- interface.py
+++ interface.py
@@ -1,19 +1,32 @@
 #Interface gráfica
 
+from function import *
 
 import PySimpleGUI as psg
 
-layout_frame = [
-    [psg.Radio('Conversão Farenheit', 'btnRadio1', key='Farenheit')],
-    [psg.Radio('Conversão Celsius', 'btnRadio 1', key='Celsius',default=True)
-]]
+layou_frame = [
+    [psg.Radio('Conversão Farenheit', 'btnRadio1', key='Farenheit', default= True)],
+    [psg.Radio('Conversão Celsius', 'btnRadio1', key='Celsius')]
+]
 
 layout = [
-    [psg.Text('Informe um número: '), psg.In(Key='numero'), psg.Frame('Opções: ', layout_frame) ],
+    [psg.Text('Informe um número: '), psg.InputText(key='numero'), psg.Frame('Opções: ', layou_frame)],
     [psg.Text("", key='resultado')],
-    [psg.Button('Calcular'), psg.Button('Limpar')],
+    [psg.Button('Calcular'), psg.Button('Limpar')]
 ]
 
 window = psg.Window('Conversor de temperatura', layout)
 
-    while True:
+while True:
+    number, valor = window.read()
+
+    if number == psg.WIN_CLOSED:
+        break
+    else:
+       if valor['Farenheit']:
+           num = C_para_F(int(valor['numero']))
+           window['resultado'].update(f'{valor["numero"]}! = {num}')
+       else:
+           window['resultado'].update(F_para_C(int(valor['numero'])))
+
+window.close()
